f87d36a8aa05d40fa1791ad431ff5d5fc07ae4f6
simpleboykrishna0 <44639656+simpleboykrishna0@users.noreply.github.com>
2025-10-06 14:28:30 +0530

    Delete setupgui.sh

--- setupgui.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Setting up GUI in Ubuntu..."
-proot-distro login ubuntu -- bash -c "
-    echo 'Installing additional GUI packages...'
-    apt update
-    apt install -y chromium-browser nano vim git curl wget
-    echo 'Additional GUI packages installed!'
-"
